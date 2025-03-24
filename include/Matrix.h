8fc32715a2d62ca0027e6f4ec3c8e3b60ead542e
OrelZabriko <orelzavriko@gmail.com>
2025-03-25 01:51:51 +0200

    Add some operators to Matrix class. need to add the operator of cin. need to check if need to fix the resrt of the operators (especially the multiplication operator) and about a constructor.

--- include/Matrix.h
+++ include/Matrix.h
@@ -12,9 +12,18 @@ public:
 	//destructor
 	~Matrix() = default;
 
-
-	void printMatrix();
+	//functions
+	const std::vector<std::vector<int>>& getMatrix() const;
+	void setMatrix(const std::vector<std::vector<int>>& matrix);
 
 private:
 	std::vector<std::vector<int>> m_matrix;
 };
+
+
+//-----operators section-----
+std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+std::istream& operator>>(std::istream& is, Matrix& matrix); //need to make this function in the cpp file
+Matrix operator+(const Matrix& left, const Matrix& right);
+Matrix operator-(const Matrix& left, const Matrix& right);
+Matrix operator*(const unsigned int mulNum, const Matrix& matrix);
--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -6,18 +6,109 @@
 //-----------------------------------------------------------------------------
 
 
+//-----------------------------------------------------------------------------
+//Get the matrix data.
+const std::vector<std::vector<int>>& Matrix::getMatrix() const
+{
+	return m_matrix;
+}
 
 
 //-----------------------------------------------------------------------------
-void Matrix::printMatrix()
+//Set the matrix data.
+void Matrix::setMatrix(const std::vector<std::vector<int>>& matrix)
 {
-	for (int row = 0; row < m_matrix.size(); row++)
+	m_matrix = matrix;
+}
+
+
+//-----------------------------------------------------------------------------
+std::ostream& operator<<(std::ostream& os, const Matrix& matrix)
+{
+	for (const auto& row : matrix.getMatrix())
 	{
-		for (int col = 0; col < m_matrix[row].size(); col++)
+		for (const auto& element : row)
 		{
-			std::cout << m_matrix[row][col] << " ";
+			os << element << " ";
+		}
+		os << std::endl;
+	}
+
+	return os;
+}
+
+
+//-----------------------------------------------------------------------------
+std::istream& operator>>(std::istream& is, Matrix& matrix)
+{
+	// TODO: insert return statement here
+}
+
+
+//-----------------------------------------------------------------------------
+Matrix operator+(const Matrix& left, const Matrix& right)
+{
+	Matrix result;
+	std::vector<std::vector<int>> tempMatrix;
+
+	for (int row = 0; row < left.getMatrix().size(); row++)
+	{
+		std::vector<int> tempRow;
+		for (int col = 0; col < left.getMatrix()[row].size(); col++)
+		{
+			tempRow.push_back(left.getMatrix()[row][col] + right.getMatrix()[row][col]);
 		}
 
-		std::cout << std::endl;
+		tempMatrix.push_back(tempRow);
 	}
+
+	result.setMatrix(tempMatrix);
+
+	return result;
+}
+
+
+//-----------------------------------------------------------------------------
+Matrix operator-(const Matrix& left, const Matrix& right)
+{
+	Matrix result;
+	std::vector<std::vector<int>> tempMatrix;
+
+	for (int row = 0; row < left.getMatrix().size(); row++)
+	{
+		std::vector<int> tempRow;
+		for (int col = 0; col < left.getMatrix()[row].size(); col++)
+		{
+			tempRow.push_back(left.getMatrix()[row][col] - right.getMatrix()[row][col]);
+		}
+
+		tempMatrix.push_back(tempRow);
+	}
+
+	result.setMatrix(tempMatrix);
+
+	return result;
+}
+
+
+//-----------------------------------------------------------------------------
+Matrix operator*(const unsigned int mulNum, const Matrix& matrix)
+{
+	Matrix result;
+	std::vector<std::vector<int>> tempMatrix;
+
+	for (int row = 0; row < matrix.getMatrix().size(); row++)
+	{
+		std::vector<int> tempRow;
+		for (int col = 0; col < matrix.getMatrix()[row].size(); col++)
+		{
+			tempRow.push_back(matrix.getMatrix()[row][col] - matrix.getMatrix()[row][col]);
+		}
+
+		tempMatrix.push_back(tempRow);
+	}
+
+	result.setMatrix(tempMatrix);
+
+	return result;
 }
