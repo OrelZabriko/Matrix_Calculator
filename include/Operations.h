5f3839f115316c03840a41424364cdf52523b810
Orel Zabriko <orelzavriko@gmail.com>
2025-03-25 12:49:49 +0200

    change name of func

--- include/Operations.h
+++ include/Operations.h
@@ -23,7 +23,7 @@ public:
 	const std::string getFunctionName() const;
 	void setFuncName(std::string name);
 	void printFuncName() const;
-	//virtual void execute(std::vector<std::vector<int>>& matrices) = 0; //need to make this function in the cpp file of the functions
+	//virtual void calcFunc(std::vector<std::vector<int>>& matrices) = 0; //need to make this function in the cpp file of the functions
 
 private:
 	std::string funcName;
