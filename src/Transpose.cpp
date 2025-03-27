01e1ca80ba6212636e49237c0622c690ce614384
Orel Zabriko <orelzavriko@gmail.com>
2025-03-27 17:59:44 +0200

    add implemantation to Transpose

--- src/Transpose.cpp
+++ src/Transpose.cpp
@@ -4,6 +4,21 @@
 
 //-----functions section------
 //-----------------------------------------------------------------------------
+Matrix Transpose::calcFunc(std::vector<Matrix> matrices)
+{
+	Matrix result(matrices[0].getSize());
+	for (int rows = 0; rows < matrices[0].getSize(); rows++)
+	{
+		for (int cols = 0; cols < matrices[0].getSize(); cols++)
+		{
+			result.setCell(rows, cols, matrices[0].getCell(cols, rows));
+		}
+	}
+	return result;
+}
+
+
+//-----------------------------------------------------------------------------
 const int Transpose::getNumberOfMatrix() const
 {
 	return 1;
